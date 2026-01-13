139d5338c9f775a61afbacebdfd154c707b8c7fe
Shaurya Kapoor <shauryakapoor2005.sk@gmail.com>
2026-01-13 20:27:24 +0530

    Basic Sorting Algos

---
+++ Day5/BubbleSort.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class BubbleSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the array: ");
+        int size = sc.nextInt();
+
+        int[] arr = new int[size];
+
+        for(int i=0 ; i<size ; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        for(int i=0 ; i<size-1 ; i++){
+            for(int j=0 ; j<size-i-1 ; j++){
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        
+        for(int i=0 ; i<size ; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }    
+}
+// Time Compelxity - O(n^2)
+// Space Complexity - O(1)
---
+++ Day5/SelectionSort.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class SelectionSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of array: ");
+        int size = sc.nextInt();
+
+        int[] arr = new int[size];
+
+        for(int i=0 ; i<arr.length ; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        for(int i=0 ; i<arr.length-1 ; i++){
+            int smallest = i;
+            for(int j=i+1 ; j<arr.length ; j++){
+                if(arr[smallest] > arr[j]){
+                    smallest = j;
+                }
+            }
+            int temp = arr[smallest];
+            arr[smallest] = arr[i];
+            arr[i] = temp;
+        }
+
+        for(int i=0 ; i<arr.length ; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
